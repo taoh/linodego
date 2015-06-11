e31e5ee1625778fa3deb7be012c94d6234e33b65
Brendan Tobolaski <brendan@tobolaski.com>
2015-06-10 22:58:27 -0400

    Export the bool value of CustomBool

--- custombool.go
+++ custombool.go
@@ -6,7 +6,7 @@ import (
 
 // CustomBool is a type to handle Linode's insistance of using ints as boolean values.
 type CustomBool struct {
-	bool
+	Bool bool
 }
 
 func (cb *CustomBool) UnmarshalJSON(b []byte) error {
@@ -14,9 +14,9 @@ func (cb *CustomBool) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("Unable to marshal value with length %d into a CustomBool.", len(b))
 	}
 	if int(b[0]) == 0 {
-		cb.bool = false
+		cb.Bool = false
 	} else if int(b[0]) == 1 {
-		cb.bool = true
+		cb.Bool = true
 	}
 	return nil
 }
@@ -25,7 +25,7 @@ func (cb *CustomBool) MarshalJSON() ([]byte, error) {
 	if cb == nil {
 		return []byte{}, fmt.Errorf("Unable to marshal nil value for CustomBool")
 	}
-	if cb.bool {
+	if cb.Bool {
 		return []byte{1}, nil
 	} else {
 		return []byte{0}, nil
