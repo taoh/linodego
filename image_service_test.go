21b03e22306d57aec19f3ee359012568095522e7
rwaweber <rwaweber@gmail.com>
2017-06-04 03:18:23 -0400

    image test now functions as standalone
    * Also added utility method to block forever until a linode's pending jobs are finished
     - this allows us to wait until the imagization is complete before we update it's info

--- image_service_test.go
+++ image_service_test.go
@@ -20,12 +20,72 @@ func TestListImages(t *testing.T) {
 
 func TestUpdateImage(t *testing.T) {
 	client := NewClient(APIKey, nil)
+	createResp, err := client.Linode.Create(2, 1, 1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// id of the freshly created linode
+	linodeId := createResp.LinodeId.LinodeId
 
-	v, err := client.Image.Update(94057, "", "Mongodb Image 2")
+	// create simple rootfs
+	_, err = client.Disk.Create(linodeId, "ext4", "rootfs", 19286, nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	image := v.Image
+	// list the disks now associated with this linode
+	diskListResp, err := client.Disk.List(linodeId, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// makes the assumption that only one disk was created
+	diskId := diskListResp.Disks[0].DiskId
+
+	// imagize the disk that was just created, unfortunately just responds with job id
+	_, err = client.Disk.Imagize(linodeId, diskId, "Part of testing", "TestingImage")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	WaitForPendingJobs(client, linodeId)
+
+	// list images for this linode account
+	imageListResp, err := client.Image.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// retrieve image
+	var imageid int
+	for _, image := range imageListResp.Images {
+		if image.Description == "Part of testing" {
+			imageid = image.ImageId
+			break
+		}
+	}
+
+	WaitForPendingJobs(client, linodeId)
+
+	imageUpdateResponse, err := client.Image.Update(imageid, "Updated label", "Updated description")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	image := imageUpdateResponse.Image
+	if image.Description != "Updated description" {
+		t.Fatal(image.Description)
+	}
 	log.Debugf("Kernel: %s, %s, %s", image.Label, image.Status, image.CreateDt)
+
+	// teardown because money
+	_, err = client.Image.Delete(imageid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = client.Linode.Delete(linodeId, true)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
