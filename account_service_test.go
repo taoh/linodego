e29cb1122e13e55a50b543ca0adbd22aa0730ab5
tamal <tamal@appscode.com>
2017-10-21 19:14:39 -0700

    Update github.com/sirupsen/logrus dependency
    ref: https://github.com/sirupsen/logrus/issues/543

--- account_service_test.go
+++ account_service_test.go
@@ -1,7 +1,7 @@
 package linodego
 
 import (
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"testing"
 )
 
--- linodego.go
+++ linodego.go
@@ -12,7 +12,7 @@ import (
 	"runtime"
 	"strings"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
